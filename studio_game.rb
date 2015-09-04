91088986f1cef6152fc964ada2ed1f30b5d4974c
Marcos Parreiras <marcosfparreiras@gmail.com>
2015-09-04 18:49:36 -0300

    Estado do código após a finalização da parte 7 do curso

--- studio_game.rb
+++ studio_game.rb
@@ -1,16 +1,39 @@
-def say_hello(name, health=100)
-	"I'm #{name.capitalize} with a health of #{health} as of #{new_time}"
+class Player
+	def initialize(name, health)
+		@name = name.capitalize
+		@health = health
+	end
+	
+	def to_s()
+		"I'm #{@name.capitalize} with a health of #{@health}"
+	end
+
+	def blam
+		@health -= 10
+	end
+
+	def w00t
+		@health += 15
+		puts "Player got w00ted"
+	end
+
+
 end
 
-def new_time
-	Time.new.strftime("%I:%M;%S");
-end
+player1 = Player.new("moe", 100)
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
 
-puts say_hello("larry", 60)
-puts say_hello("curly", 125)
-puts say_hello("moe")
-puts say_hello("shemp", 90)
+player3.blam
+puts player3
+player3.w00t
+puts player3
 
-puts new_time
+# puts player1.inspect
+
+# puts say_hello("moe")
+# puts say_hello("shemp", 90)
+
+# puts new_time
 
 # puts say_hello

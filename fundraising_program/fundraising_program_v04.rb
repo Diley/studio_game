900730a02b3c8a7d8eb65054cc65dd0a1d0df95c
Marcos Parreiras <marcosfparreiras@gmail.com>
2015-09-08 14:47:38 -0300

    Criada versão do programa para fundraising referente ao exercício 6 do curso de Ruby

--- fundraising_program/fundraising_program_v04.rb
+++ fundraising_program/fundraising_program_v04.rb
@@ -1,23 +1,24 @@
 # Foundraising Program
 # Dados baseados em https://wefunder.me/
 
-project1 = 'Parkav'	
+project1 = 'parkav'	
 funding1 = 669000
 description1 = 'projeto para encontrar locais de estacionamento com 95% de precisao'
 
-project2 = 'Plate IQ'	
+project2 = 'plate iq'	
 funding2 = 654000
 description2 = 'plataforma de software que permite que restaurantes armazenem e gerenciem todos os pedidos e contas na nuvem e monitora seus lucros e prejuizos em tempo real'
 
 description3 = 'corporacao de construcao de foguetes especificamente para lancar nanossatelites, 1 do tamanho de um foguete padrao ground-to-orbit. Promete lancar 50 foguetes por ano'
-project3 = 'Bagaveev'
+project3 = 'bagaveev'
 funding3 = 1128600
 
 description4 = 'projeto para que se possa descobrir e comprar bebidas spirit online'
-project4 = 'Flaviar'
+project4 = 'flaviar'
 funding4 = 2912200
 
+puts "\n#{project1.capitalize} teve investimento de #{funding1}."
+puts "\n#{project2.upcase} teve investimento de #{funding2}."
+puts "\n" + "#{project3.capitalize} teve investimento de #{funding3}".center(80,'- * ')
+puts "\n" + "#{project4.upcase} teve investimento de #{funding4}.".rjust(70)
 
-puts "Projeto #{project1}, #{description1}, teve R$ #{funding1} de investimento."
-
-puts "\nProjetos:\n\tProjeto #{project1}\n\tProjeto #{project2}\n\tProjeto #{project3}\n\tProjeto #{project4}"
--- fundraising_program/fundraising_program_v06.rb
+++ fundraising_program/fundraising_program_v06.rb
@@ -1,23 +1,27 @@
 # Foundraising Program
 # Dados baseados em https://wefunder.me/
 
-project1 = 'Parkav'	
+def print_project_funding( project, description, funding = 100000 )
+	"O projeto #{project}, #{description}, recebeu um investimento de R$ #{funding}."
+end
+
+project1 = 'parkav'	
 funding1 = 669000
 description1 = 'projeto para encontrar locais de estacionamento com 95% de precisao'
 
-project2 = 'Plate IQ'	
+project2 = 'plate iq'	
 funding2 = 654000
 description2 = 'plataforma de software que permite que restaurantes armazenem e gerenciem todos os pedidos e contas na nuvem e monitora seus lucros e prejuizos em tempo real'
 
 description3 = 'corporacao de construcao de foguetes especificamente para lancar nanossatelites, 1 do tamanho de um foguete padrao ground-to-orbit. Promete lancar 50 foguetes por ano'
-project3 = 'Bagaveev'
+project3 = 'bagaveev'
 funding3 = 1128600
 
 description4 = 'projeto para que se possa descobrir e comprar bebidas spirit online'
-project4 = 'Flaviar'
+project4 = 'flaviar'
 funding4 = 2912200
 
-
-puts "Projeto #{project1}, #{description1}, teve R$ #{funding1} de investimento."
-
-puts "\nProjetos:\n\tProjeto #{project1}\n\tProjeto #{project2}\n\tProjeto #{project3}\n\tProjeto #{project4}"
+puts print_project_funding( project1, description2, funding1 )
+puts print_project_funding( project2, description2, funding2 )
+puts print_project_funding( project3, description3, funding3 )
+puts print_project_funding( project4, description4 )

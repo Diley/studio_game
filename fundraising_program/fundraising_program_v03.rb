beea365298c71b2f7f1a563ad6d096fb04a1c09d
Marcos Parreiras <marcosfparreiras@gmail.com>
2015-09-08 14:29:52 -0300

    Início do desenvolvimento do programa relacionado a fundraising. O número da versão será sempre referente ao arquivo pdf de exercícios ao qual a versão é referente. Neste caso, a versão é a terceira

---
+++ fundraising_program/fundraising_program_v03.rb
@@ -0,0 +1,23 @@
+# Foundraising Program
+# Dados baseados em https://wefunder.me/
+
+project1 = 'Parkav'	
+funding1 = 669000
+description1 = 'projeto para encontrar locais de estacionamento com 95% de precisao'
+
+project2 = 'Plate IQ'	
+funding2 = 654000
+description2 = 'plataforma de software que permite que restaurantes armazenem e gerenciem todos os pedidos e contas na nuvem e monitora seus lucros e prejuizos em tempo real'
+
+description3 = 'corporacao de construcao de foguetes especificamente para lancar nanossatelites, 1 do tamanho de um foguete padrao ground-to-orbit. Promete lancar 50 foguetes por ano'
+project3 = 'Bagaveev'
+funding3 = 1128600
+
+description4 = 'rojeto para que se possa descobrir e comprar bebidas spirit online'
+project4 = 'Flaviar'
+funding4 = 2912200
+
+
+puts "Projeto #{project1}, #{description1}, teve R$ #{funding1} de investimento."
+
+puts "\nProjetos:\n\tProjeto #{project1}\n\tProjeto #{project2}\n\tProjeto #{project3}\n\tProjeto #{project4}"
